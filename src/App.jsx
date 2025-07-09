36d1d7d3e351ff13cfc17d2329f6d7c7427823f7
codewithsaidul <codewithsaidul@gmail.com>
2025-07-09 21:41:57 +0600

    first gsap project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div className="flex-center h-screen">
+        <h1 className="text-7xl text-rose-500">Hello, GSAP</h1>
+    </div>
+  )
+}
+
+export default App
